d16681c7accda65789be9604425954ff80f08b54
kam <kam@kamm.ru>
2020-04-05 12:50:33 +0500

    config

--- tools/kam.sh
+++ tools/kam.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 apt install -y git-core zsh fonts-powerline &&
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh &&
+wget https://raw.githubusercontent.com/korzunin/ohmyzsh/master/tools/install.sh &&
 REPO=korzunin/ohmyzsh sh install.sh
